3d02a11e241e10e318897a3ae1fa4763b8ca10a6
Thomas GRANDJEAN <thomas.grandjean@epitech.eu>
2018-11-01 13:46:41 +0100

    Add main.dart file (EntryPoint of the App)

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:dev_epicture_2018/ui/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
+    runApp(new MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        primaryColor: Colors.black,
+        accentColor: Colors.white,
+      ),
+      home: new HomePage(),
+    );
+  }
+}
