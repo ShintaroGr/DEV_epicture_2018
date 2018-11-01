1c05b414b0ca3f9bce6404060b4676f5b506b819
Thomas GRANDJEAN <thomas.grandjean@epitech.eu>
2018-11-01 13:47:08 +0100

    Add imgur object

---
+++ lib/data/imgur.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+class Imgur {
+  final String id;
+  final String title;
+  final String link;
+  final String cover;
+  final bool isAlbum;
+  final int commentCount;
+  int favoriteCount;
+  int points;
+  String vote;
+
+  Imgur({this.id, this.title, this.link, this.cover, this.isAlbum, this.commentCount, this.favoriteCount, this.points, this.vote});
+
+  factory Imgur.fromJson(Map<String, dynamic> json) {
+    return Imgur(
+        id: json['id'],
+        title: json['title'],
+        link: json['link'],
+        cover: json['cover'],
+        isAlbum: json['is_album'],
+        commentCount: json['comment_count'],
+        favoriteCount: json['favorite_count'],
+        points: json['points'],
+        vote: json['vote']);
+  }
+
+  static List<Imgur> allFromResponse(String response) {
+    var decodedJson = json.decode(response).cast<String, dynamic>();
+
+    return decodedJson['data'].cast<Map<String, dynamic>>().map((obj) => Imgur.fromJson(obj)).toList().cast<Imgur>();
+  }
+
+  static List<Imgur> allFromResponseTag(String response) {
+    var decodedJson = json.decode(response).cast<String, dynamic>();
+
+    return decodedJson['data']['items'].cast<Map<String, dynamic>>().map((obj) => Imgur.fromJson(obj)).toList().cast<Imgur>();
+  }
+}
